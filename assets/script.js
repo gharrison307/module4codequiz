2f5db5d63f5de8fe42fb71601365814dc3b9bdc5
George Harrison <g.a.harrison91@gmail.com>
2023-02-16 15:23:27 -0700

    added question arrays

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,26 @@ var startButtonEl = $("#startButton");
 var outputResponseEl = $("outputResponse");
 
 var score = 0;
+
+var question1 [
+{
+questionline = "What is the capital of Colorado?",
+answers = ["Denver", "Cheyenne", "Colorado Springs", "Boulder"],
+correctAnswer = "Denver"
+}
+]
+var question2 [
+    {
+    questionline = "What is the State Bird of Colorado?",
+    answers = ["Cactus Wren", "American Robin", "Wood Thrush", "Lark Bunting"],
+    correctAnswer = "Lark Bunting"
+    }
+    ]
+
+var question3 [
+        {
+        questionline = "What is the longest river in the world?",
+        answers = ["The Yangtze", "The Amazon", "The Nile", "The Mississippi"],
+        correctAnswer = "The Nile"
+        }
+        ]
